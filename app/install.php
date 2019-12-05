fde137eaa77f94ca869d0f568e3db39b9791da36
chuckrut <jesusdp24@gmail.com>
2019-12-05 10:38:49 +0100

    cambio2

--- app/install.php
+++ app/install.php
@@ -1,16 +1,13 @@
 <?php
 include("../seguridad/conexion.inc.php");
-$con=new mysqli($mysql_server,$mysql_login,$mysql_pass);
+$con=new mysqli($mysql_server,$mysql_login,$mysql_pass,$mysql_db);
 
 if ($con->connect_error) {
     die('Error de Conexión (' . $con->connect_errno . ') '. $con->connect_error);
 }
 else{
 
-    if ($con->query("CREATE DATABASE alimentos") === TRUE) {
-    printf("Se creó con éxtio la base de datos alimentos.\n");
-    }
-    if ($con->query("USE alimentos") === TRUE) {
+    if ($con->query("USE alimentos42019") === TRUE) {
     printf("Usando la base de datos alimentos.\n");
     }
     include("../seguridad/sentencias.sql.php");
